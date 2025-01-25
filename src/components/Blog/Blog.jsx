ebc8d9c7c8f4543f84422e0e0dc301fdc0a9a4a2
unknown <nahid455walton@gmail.com>
2025-01-25 17:47:27 +0600

    Your commit message here

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 const Blog = ({ blog }) => {
     const {title, cover, author, author_img, reading_time,posted_date,hashtags}=blog;
     return (
-        <div>
-            <img className='w-80' src={cover} alt={`cover pic of title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`cover pic of title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-14' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -14,6 +15,9 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time} min raed</span>
+                    <button 
+                        className='ml-2 text-red-600 text-2xl'
+                    ><FaBookmark></FaBookmark> </button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
